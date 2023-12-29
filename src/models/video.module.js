3f1a73381dbdbeb09d9f23ac3ca1f79b30e1f959
B. Deewakar Rao <b.deewakarrao@Bs-MacBook-Air.local>
2023-12-30 02:25:25 +0530

    created user and video model

---
+++ src/models/video.module.js
@@ -0,0 +1,40 @@
+import { Schema , model } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchmea = new Schema({
+    videoFile:{
+        type: String, //cloudinary url
+        required: true
+    },
+    thumbnail:{
+        type: String, //cloudinary url
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String, 
+        required: true
+    },
+    duration:{
+        type: Number,
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {timestamps: true})
+
+videoSchmea.plugin(mongooseAggregatePaginate);
+export const Video = model("Video", videoSchmea);
