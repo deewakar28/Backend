7c99791ba9bab2e3a7d879eb5af638c5c0789f2b
B. Deewakar Rao <b.deewakarrao@Bs-MacBook-Air.local>
2024-01-02 18:43:18 +0530

    added refresh token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { logOutUser, registerUser, loginUser } from "../controllers/user.controller.js";
+import { logOutUser, registerUser, loginUser, refreshAccessToken } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -21,6 +21,9 @@ userRouter.route("/register").post(
 
 userRouter.route("/login").post(loginUser);
 
+//secured routes
 userRouter.route("/logout").post(verifyJWT, logOutUser);
+
+userRouter.route("/refresh-token").post(refreshAccessToken);
 
 export default userRouter;
