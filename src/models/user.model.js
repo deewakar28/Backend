b5dfb7f873bb31c0b93fd95ea4475c6e7965b957
B. Deewakar Rao <deewakarrao59@gmail.com>
2024-01-06 16:20:04 +0530

    added changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,7 +65,7 @@ userSchema.methods.isPasswordCorrect = async function(password){
 userSchema.methods.generateAccessToken = function()
 {
    return jwt.sign({
-        _id: this.id,
+        _id: this._id,
         email: this.email,
         userName: this.userName,
         fullName: this.fullName
@@ -79,7 +79,7 @@ userSchema.methods.generateAccessToken = function()
 userSchema.methods.generateRefreshToken = function()
 {
     return jwt.sign({
-        _id: this.id,
+        _id: this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
     {
